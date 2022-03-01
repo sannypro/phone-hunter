560c7897e7a09f68ba55c6bb2c77e06eca4c8e7a
sannypro <sannymr79@gmail.com>
2022-03-01 16:31:17 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -87,19 +87,19 @@ const phoneDetail = (slug) => {
                                     <p><b>Name: </b>${data.name}</p>
                                     <p>Release Date: no result found</p>
                                     <h1>Main Feature</h1>
-                                    <p><b>Chipset:</b> ${data.mainFeatures.chipSet}</p>
-                                    <p ><b>Display Size:</b> ${data.mainFeatures.displaySize}</p>
-                                    <p ><b>Memory:</b> ${data.mainFeatures.memory}</p>
-                                    <p ><b>Storage:</b> ${data.mainFeatures.storage}</p>
+                                    <p><b>Chipset:</b> ${data?.mainFeatures?.chipSet}</p>
+                                    <p ><b>Display Size:</b> ${data?.mainFeatures?.displaySize}</p>
+                                    <p ><b>Memory:</b> ${data?.mainFeatures?.memory}</p>
+                                    <p ><b>Storage:</b> ${data?.mainFeatures?.storage}</p>
                                     <h5> Some sensors :</h5>
-                                    <p>${data.mainFeatures.sensors}</p>
+                                    <p>${data?.mainFeatures?.sensors}</p>
                                     <h2> Other info :</h2>
-                                    <p><b>Bluetooth:</b> ${data.others.Bluetooth}</p>
-                                    <p><b>GPS: </b>${data.others.GPS}</p>
-                                    <p><b>NFC: </b>${data.others.NFC}</p>
-                                    <p><b>Radio: </b> ${data.others.Radio}</p>
-                                    <p><b>USB: </b>${data.others.USB}</p>
-                                    <p><b>WLAN:</b> ${data.others.WLAN}</p>
+                                    <p><b>Bluetooth:</b> ${data?.others?.Bluetooth}</p>
+                                    <p><b>GPS: </b>${data?.others?.GPS}</p>
+                                    <p><b>NFC: </b>${data?.others?.NFC}</p>
+                                    <p><b>Radio: </b> ${data?.others?.Radio}</p>
+                                    <p><b>USB: </b>${data?.others?.USB}</p>
+                                    <p><b>WLAN:</b> ${data?.others?.WLAN}</p>
                                 </div>
                                 <button onclick='closeDetail()' class="btn btn-primary w-50 mx-auto"> close </button>
             </div>
@@ -140,23 +140,23 @@ const phoneDetail = (slug) => {
                                     <img class=" w-25 " src="${data.image}" alt="Card image cap">
                                 </div>
                                 <div class="w-75 mx-auto">
-                                    <h5>${data.brand}</h5>
-                                    <p><b>Name: </b>${data.name}</p>
-                                    <p>Release Date: ${data.releaseDate}</p>
+                                    <h5>${data?.brand}</h5>
+                                    <p><b>Name: </b>${data?.name}</p>
+                                    <p>Release Date: ${data?.releaseDate}</p>
                                     <h1>Main Feature</h1>
-                                    <p><b>Chipset:</b> ${data.mainFeatures.chipSet}</p>
-                                    <p ><b>Display Size:</b> ${data.mainFeatures.displaySize}</p>
-                                    <p ><b>Memory:</b> ${data.mainFeatures.memory}</p>
-                                    <p ><b>Storage:</b> ${data.mainFeatures.storage}</p>
+                                    <p><b>Chipset:</b> ${data?.mainFeatures?.chipSet}</p>
+                                    <p ><b>Display Size:</b> ${data.mainFeatures?.displaySize}</p>
+                                    <p ><b>Memory:</b> ${data.mainFeatures?.memory}</p>
+                                    <p ><b>Storage:</b> ${data.mainFeatures?.storage}</p>
                                     <h2> Some sensors :</h2>
-                                    <p>${data.mainFeatures.sensors}</p>
+                                    <p>${data.mainFeatures?.sensors}</p>
                                     <h2> Other info :</h2>
-                                    <p><b>Bluetooth:</b> ${data.others.Bluetooth}</p>
-                                    <p><b>GPS: </b>${data.others.GPS}</p>
-                                    <p><b>NFC: </b>${data.others.NFC}</p>
-                                    <p><b>Radio: </b> ${data.others.Radio}</p>
-                                    <p><b>USB: </b>${data.others.USB}</p>
-                                    <p><b>WLAN:</b> ${data.others.WLAN}</p>
+                                    <p><b>Bluetooth:</b> ${data?.others?.Bluetooth}</p>
+                                    <p><b>GPS: </b>${data?.others?.GPS}</p>
+                                    <p><b>NFC: </b>${data?.others?.NFC}</p>
+                                    <p><b>Radio: </b> ${data?.others?.Radio}</p>
+                                    <p><b>USB: </b>${data?.others?.USB}</p>
+                                    <p><b>WLAN:</b> ${data?.others?.WLAN}</p>
                                     
                                 </div>
                                 <button onclick='closeDetail()' class="btn btn-primary w-50 mx-auto"> close </button>
